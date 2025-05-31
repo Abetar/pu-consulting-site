69a2844fe1a3a08ab551cc83282798d08b1cf2a2
Abetar <alexisgomez_96@hotmail.com>
2025-05-31 14:30:56 -0600

    Adding an HTML head for google search console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,13 @@ const inter = Inter({
   variable: "--font-inter",
 })
 
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" className={inter.variable}>
+      <head>
+        {/* Google Search Console Verification */}
+        <meta name="google-site-verification" content="e4o1fhgJCysu2DQLONjF4LSsZDTIEFF6VW0mVLT1Jhw" />
+      </head>
       <body className="min-h-screen flex flex-col font-sans">
         <Navbar />
         <main className="flex-grow mt-[72px]">{children}</main>
